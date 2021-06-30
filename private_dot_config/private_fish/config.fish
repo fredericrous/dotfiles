5bf7181024f1da4b0dab64e7517d24bff906b85c
fredericrous <frederic.rousseau.fr@gmail.com>
2021-06-30 03:33:29 +0100

    Add touchbar script and other stuff

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -10,12 +10,13 @@ if test (uname -s) = "Darwin"
 
   set -gx ANDROID_HOME /Users/fredericrous/Library/AndroidSDK
   set -gx JAVA_HOME /usr/local/opt/openjdk
-  set -gx EDITOR /usr/bin/vim
-  set -gx MANPAGER "sh -c 'col -bx | bat -l man -p'"
-  set -gx RIPGREP_CONFIG_PATH "$HOME/.ripgreprc"
 end
 
+set -gx EDITOR /usr/bin/vim
+set -gx MANPAGER "sh -c 'col -bx | bat -l man -p'"
+set -gx RIPGREP_CONFIG_PATH "$HOME/.ripgreprc"
 set fish_greeting ""
+fish_vi_key_bindings
 
 if test "$COLORTERM" = "truecolors"
   set -gx LS_COLORS (vivid generate lava)
@@ -25,6 +26,7 @@ end
 
 source ~/.iterm2_shell_integration.(basename $SHELL)
 
-fish_vi_key_bindings
+set -gx LANG en_US.UTF-8
+set -gx LC_ALL en_US.UTF-8
 
 starship init fish | source
---
+++ private_dot_config/private_fish/functions/__fish_apple_touchbar_bind_key_custom.fish
@@ -0,0 +1,17 @@
+function __fish_apple_touchbar_bind_key_custom --argument-names fn_number fn_text fn_command bind_option
+    __fish_apple_touchbar_print_key "SetKeyLabel=F$fn_number=$fn_text"
+
+    if ! test "$fish_key_bindings" = fish_default_key_bindings
+        set vimbind "--mode" "insert"
+    end
+
+    if [ "$bind_option" = "-s" ]
+        if string match -q -- "*osascript*" "$fn_command"
+	    bind $vimbind --key f$fn_number "eval ($fn_command)"
+        else
+            bind $vimbind --key f$fn_number "commandline --replace '$fn_command'; commandline --function execute"
+        end
+    else
+        bind --key f$fn_number $fn_command
+    end
+end
