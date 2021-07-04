06946a8ad670f4723c787e485d583982a185f2f1
fredericrous <frederic.rousseau.fr@gmail.com>
2021-07-04 20:20:51 +0100

    continue work on XDG + few modifs

--- private_dot_config/private_fish/conf.d/alias.fish
+++ private_dot_config/private_fish/conf.d/alias.fish
@@ -5,3 +5,7 @@ alias diff='delta'
 alias fd='fd -H'
 alias ping='prettyping'
 alias ssh='mosh'
+
+function code2img --argument-names language 
+    silicon --from-clipboard -l $language --to-clipboard
+end
