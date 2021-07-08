81ad613950ee2f44077fa415e4777a037311b62e
fredericrous <frederic.rousseau.fr@gmail.com>
2021-07-08 21:45:33 +0100

    🔧  chore: updates + run chmod on git hooks

--- dot_prettierrc.js
+++ dot_prettierrc.js
@@ -1,6 +1,7 @@
 'use strict';
 
 module.exports = {
+  tabWidth: 2,
   trailingComma: 'es5',
   singleQuote: true,
   printWidth: 100,
---
+++ private_dot_config/git/run_after_git-hooks-setup.zsh
@@ -0,0 +1,2 @@
+#/bin/zsh
+chmod +x "${XDG_CONFIG_HOME:-~/.config}/git/templates/hooks/*"
