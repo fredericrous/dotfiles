bbb056b279fa95ef8e295504102054ae0a171fb6
Frederic R <frederic.rousseau.fr@gmail.com>
2022-06-20 18:24:02 +0100

    Update config.fish
    starship needs to be before is-interactive ... idk why

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,13 +1,14 @@
+starship init fish | source
 
 ###################################
 # Interactive mode configurations #
 ###################################
 status is-interactive || exit
 
-starship init fish | source
 set -x GPG_TTY (tty)
-source $HOME/.iterm2_shell_integration.(basename $SHELL)
 fish_vi_key_bindings
 fzf_configure_bindings --directory=\cf --git_log=\cg --git_status=\cs
+
+source $HOME/.iterm2_shell_integration.(basename $SHELL)
 
 set fish_greeting ""
