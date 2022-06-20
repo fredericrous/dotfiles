44cf3819ca612b77f429527814d413aa746d324a
Frederic R <frederic.rousseau.fr@gmail.com>
2022-06-20 18:20:42 +0100

    Update run_after_git-hooks-chmod+x.zsh

--- private_dot_config/git/run_after_git-hooks-chmod+x.zsh
+++ private_dot_config/git/run_after_git-hooks-chmod+x.zsh
@@ -1,3 +1,4 @@
 #!/bin/sh
 chmod +x "${XDG_CONFIG_HOME:-~/.config}"/git/git-templates/templates/hooks/* || true
 
+# run_once hash: {{ include "private_dot_config/git/git-templates/templates/hooks" | sha256sum }}
